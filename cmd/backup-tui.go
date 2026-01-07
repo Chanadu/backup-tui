2754f2ac8070fe1c196cb41e4c502b91998d459d
chanadu <chandu.peddada@gmail.com>
2026-01-07 00:19:08 -0600

    bubbles textinput

--- cmd/backup-tui.go
+++ cmd/backup-tui.go
@@ -5,66 +5,81 @@ import (
 	"os"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct {
-	// choices  []string
-	// cursor   int
-	// selected map[int]struct{}
-	user   string
-	server string
+	input textinput.Model
+	done  bool
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 
 	case tea.KeyMsg:
 		switch msg.String() {
 
-		case "ctrl+c":
+		case "esc", "ctrl+c":
 			return m, tea.Quit
-		case "backspace":
-			if len(m.user) > 0 {
-				m.user = m.user[:len(m.user)-1]
-			}
-
-		case "enter", " ":
-			fmt.Printf("Entered User: %s", m.user)
+		case "enter":
+			m.done = true
 			return m, tea.Quit
-		default:
-			m.user += msg.String()
 		}
+
 	}
 
-	return m, nil
+	m.input, cmd = m.input.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
 	var s strings.Builder
 
 	// Send the UI for rendering
-	fmt.Fprintf(&s, "User: %s", m.user)
+	// fmt.cprintf(&s, "User: %s", m.user)
+
+	if m.done {
+		return fmt.Sprintf(
+			"You typed: %s\n",
+			m.input.Value(),
+		)
+	}
+
+	fmt.Fprintf(&s, "Enter text:\n%s\n(Press Enter to submit)", m.input.View())
+
 	return s.String()
+}
+
+func initialModel() model {
+	ti := textinput.New()
+
+	ti.Placeholder = "Enter Server User"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 30
+
+	m := model{
+		input: ti,
+	}
+
+	return m
 }
 
 func Start() {
 	fmt.Println("BackupTui")
 
-	m := model{
-		user:   "",
-		server: "",
-	}
-
-	p := tea.NewProgram(m)
+	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		_ = fmt.Errorf("error: %v", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("User: %s", m.user)
+	// fmt.Printf("User: %s", m.user)
 }

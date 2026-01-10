293fe759d769371bdb360f529a5c37bfb89a33a5
chanadu <chandu.peddada@gmail.com>
2026-01-09 21:26:02 -0600

    change debug stuff

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/Chanadu/backup-tui/cmd"
 	tea "github.com/charmbracelet/bubbletea"
@@ -11,7 +12,17 @@ import (
 func main() {
 	if len(os.Getenv("DEBUG")) > 0 {
 		fmt.Println("DEBUG MODE")
-		f, err := tea.LogToFile("debug.log", "debug: ")
+		f, err := tea.LogToFile("debug.log", "")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+
+		defer func() {
+			_ = f.Close()
+		}()
+	} else {
+		f, err := tea.LogToFile(fmt.Sprintf("%s.log", time.Now().Format("2006-01-02_15-04-05")), "debug: ")
 		if err != nil {
 			fmt.Println("fatal:", err)
 			os.Exit(1)
